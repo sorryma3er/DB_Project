1e08b3106d502a238ddbc75d95e4af1a07ba23d0
sorryma3er <whc0713air@gmail.com>
2024-12-06 02:42:22 +0700

    Made some changes since the structure of this database changed

--- Data2.sql
+++ Data2.sql
@@ -20,50 +20,6 @@ CREATE TRIGGER calculate_total_price
     FOR EACH ROW
 EXECUTE FUNCTION update_total_price();
 
-INSERT INTO Sales (branch_id, sale_date, total_price)
-VALUES
-    (1, '2024-12-01', 0),  -- Sale ID 1
-    (1, '2024-12-02', 0),  -- Sale ID 2
-    (1, '2024-12-03', 0),  -- Sale ID 3
-    (2, '2024-12-01', 0),  -- Sale ID 4
-    (2, '2024-12-02', 0),  -- Sale ID 5
-    (2, '2024-12-03',0);   -- Sale ID 6
-
-INSERT INTO Sales_Detail (sale_id, product_id, quantity, subtotal)
-VALUES
-    (1, 1, 1, 37900.00),  -- MacBook Air (1 unit)
-    (1, 15, 2, 4980.00), -- Razer DeathAdder (2 units)
-    (1, 23, 1, 4990.00); -- Corsair K70 (1 unit)
-
-INSERT INTO Sales_Detail (sale_id, product_id, quantity, subtotal)
-VALUES
-    (2, 2, 1, 25900.00), -- Acer Swift 3 (1 unit)
-    (2, 27, 2, 5980.00), -- SteelSeries Rival (2 units)
-    (2, 6, 1, 29900.00); -- Samsung Galaxy Book (1 unit)
-
-INSERT INTO Sales_Detail (sale_id, product_id, quantity, subtotal)
-VALUES
-    (3, 8, 1, 44900.00), -- Samsung Galaxy Z Flip 5 (1 unit)
-    (3, 15, 1, 2490.00), -- Razer DeathAdder (1 unit)
-    (3, 24, 1, 1990.00); -- Laptop Cooling Pad (1 unit)
-
-INSERT INTO Sales_Detail (sale_id, product_id, quantity, subtotal)
-VALUES
-    (4, 1, 1, 37900.00), -- MacBook Air (1 unit)
-    (4, 22, 1, 11000.00), -- Secretlab Titan Evo (1 unit)
-    (4, 31, 2, 2990.00); -- Wireless Charger (2 units)
-
-INSERT INTO Sales_Detail (sale_id, product_id, quantity, subtotal)
-VALUES
-    (5, 2, 1, 25900.00), -- Acer Swift 3 (1 unit)
-    (5, 8, 1, 44900.00), -- Samsung Galaxy Z Flip 5 (1 unit)
-    (5, 27, 3, 8970.00); -- SteelSeries Rival (3 units)
-
-INSERT INTO Sales_Detail (sale_id, product_id, quantity, subtotal)
-VALUES
-    (6, 6, 1, 29900.00), -- Samsung Galaxy Book (1 unit)
-    (6, 24, 1, 1990.00), -- Laptop Cooling Pad (1 unit)
-    (6, 15, 2, 4980.00); -- Razer DeathAdder (2 units)
 
 
 
